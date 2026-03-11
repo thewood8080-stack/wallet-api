a17b177982e9296cbf802d4d91b2347b1a9ee87e
yosef <the.wood.8080@gmail.com>
2026-03-12 00:18:44 +0200

    remove unused coupon and ai files, add mobile back button, expiry alert

--- api/v1/routes/ai.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { analyzeImage, askAI } = require('../controllers/ai');
-const authMiddleware = require('../middlewares/auth');
-
-router.post('/analyze', authMiddleware, analyzeImage);
-router.post('/ask', authMiddleware, askAI);
-
-module.exports = router;
